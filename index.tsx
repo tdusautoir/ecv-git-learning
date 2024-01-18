0f7af02ff5206099bd6cc5293361970a009e5cf7
tdusautoir <tdnet59@gmail.com>
2024-01-18 14:47:13 +0100

    feature(swag): change theme color

--- index.tsx
+++ index.tsx
@@ -3,10 +3,21 @@ import { createRoot } from 'react-dom/client';
 import { Deck, DefaultTemplate, MarkdownSlideSet } from 'spectacle';
 import mdContent from './slides.md';
 
-const Presentation = () => (
-  <Deck template={() => <DefaultTemplate />}>
-    <MarkdownSlideSet>{mdContent}</MarkdownSlideSet>
-  </Deck>
-);
+const Presentation = () => {
+  const theme = {
+    colors: {
+      primary: '#ffffff',
+      secondary: '#dfba19',
+      tertiary: '#051730',
+      quaternary: '#2563eb',
+      quinary: '#000'
+    },
+  };
+  return ( 
+    <Deck template={() => <DefaultTemplate />} theme={theme}>
+      <MarkdownSlideSet>{mdContent}</MarkdownSlideSet>
+    </Deck>
+  )
+};
 
 createRoot(document.getElementById('app')!).render(<Presentation />);
